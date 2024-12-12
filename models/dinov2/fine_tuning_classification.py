7a679cc838660adbebbdd238adb2daf196268ec9
Phat Tran <phat.tran.developer@gmail.com>
2024-12-12 17:58:09 +0700

    Add fine-tuning code

--- models/dinov2/fine_tuning_classification.py
+++ models/dinov2/fine_tuning_classification.py
@@ -157,12 +157,13 @@ class AugmentedDINOv2Dataset(Dataset):
         # Define augmentation transforms
         self.augment_transform = transforms.Compose([
             ResizeAndPad((256, 256), 14),
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomRotation(15),
+            # transforms.RandomHorizontalFlip(p=0.5),
+            # transforms.RandomRotation(15),
             transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
-            transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
+            # transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
             transforms.ToTensor(),
-            transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+            transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            transforms.RandomErasing()
         ])
 
         self.based_transform = transforms.Compose([
