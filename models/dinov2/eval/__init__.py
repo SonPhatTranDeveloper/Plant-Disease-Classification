be6b2f205e95a7385cbb11ccb0879259192f3571
Phat Tran <phat.tran.developer@gmail.com>
2024-12-12 16:11:02 +0700

    Added template

--- models/dinov2/eval/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Author: Son Phat Tran
-This file contains the code for linear classification head of DINOv2 model
-"""
--- models/dinov2/fine_tuning_classification.py
+++ models/dinov2/fine_tuning_classification.py
@@ -2,3 +2,5 @@
 Author: Son Phat Tran
 This code contains the code fine-tuning DINOv2, classification style.
 """
+
+
